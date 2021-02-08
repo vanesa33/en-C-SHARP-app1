7d7ccd1387ebb2de7883d6fadc26e44830857bca
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 14:48:21 -0300

    Add files via upload

---
+++ AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("App1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("App1")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2020")>
+<Assembly: AssemblyTrademark("")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Ensamblado: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: ComVisible(False)>
---
+++ MainPage.xaml.vb
@@ -0,0 +1,9 @@
+﻿' La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+''' <summary>
+''' Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+End Class
